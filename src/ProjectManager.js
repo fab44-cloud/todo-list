3315a1135d2b8a12bf0d9e687a1a7146449cab40
Ford Berry <fordaberry@gmail.com>
2025-09-15 22:04:58 -0600

    Create a 'default' project

--- src/ProjectManager.js
+++ src/ProjectManager.js
@@ -1,7 +1,12 @@
 // Application logic
+import { Project } from './Project';
 
 let projects = [];
 
-addProject() {
-
-};
+const createDefaultProject = () => {
+    // Check if projects array is empty, and if so, add a default project
+    if (projects.length === 0) {
+        const defaultProject = new Project('Default Project');
+        projects.push(defaultProject);
+    }
+}
