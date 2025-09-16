fb3b69bfd6d9c83cc29146951ba19af2c3de055c
Ford Berry <fordaberry@gmail.com>
2025-09-16 00:59:41 -0600

    Add and remove todo items

--- src/Project.js
+++ src/Project.js
@@ -1,11 +1,34 @@
 // Data model
-import Todo from './Todo';
+import { Todo } from './Todo';
 
 class Project {
     constructor(name) {
         this.name = name;
         this.id = Date.now();
         this.todos = [];
+    }
+
+    addTodo(todo) {
+        if (todo instanceof Todo) {
+            this.todos.push(todo);
+        } else {
+            console.error('Invalid item. Must be an instance of Todo.')
+        }
+    }
+
+    // Remove a todo item by its ID
+    removeTodo(todoId) {
+        this.todos = this.todos.filter(todo => todo.id !== todoId)   
+    }
+
+    // Get all todo items in this project
+    getTodos() {
+        return [...this.todos];
+    }
+
+    // Find a specific todo item by its ID
+    findTodos(todoId) {
+        return this.todos.find(todo => todo.id === todoId);
     }
 }
 
