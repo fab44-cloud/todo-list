6178dbf4519e76ca58d49eab5723727431474d93
Ford Berry <fordaberry@gmail.com>
2025-09-17 21:51:29 -0600

    Create function to view all projects

--- src/ui.js
+++ src/ui.js
@@ -1,1 +1,22 @@
 // Presentation layer (DOM manipulation)
+import { createDefaultProject, getProjects, addProject} from './ProjectManager';
+import { Todo } from './Todo'
+
+const projectListElement = document.getElementById('project-list');
+
+// View all projects
+export function renderProjects(projects){
+    projectListElement.innerHTML = "";
+    
+    projects.forEach(project => {
+        const listItem = document.createElement('li');
+        listItem.textContent = project.name;
+        listItem.dataset.projectId = project.id;
+
+        if (project.id === activeProjectId){
+            listItem.classList.add('active');
+        }
+        
+        projectListElement.appendChild(listItem);
+    });
+}
