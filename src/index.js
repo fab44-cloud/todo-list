af8a6679dd71fea2cd8a32d83ef95ed98f1b711c
Ford Berry <fordaberry@gmail.com>
2025-09-20 15:55:44 -0600

    Define handler functions that interact with ProjectManager.JS

--- src/index.js
+++ src/index.js
@@ -29,6 +29,35 @@ function onAddProject(projectName) {
     renderAll();
 }
 
+function onSelectProject(projectId) {
+    activeProjectId = projectId;
+    renderAll();
+}
+
+function onAddTodo(todoTitle) {
+    if (!activeProjectId) {
+        return 'Please select a project first.';   
+    }
+    ProjectManager.addTodoToProject(activeProjectId, todoTitle);
+    renderAll();    
+}
+
+function onSaveTodo(todoData) {
+    ProjectManager.updateTodo(activeProjectId, todoData);
+    UI.hideTodoModal();
+    renderAll();
+}
+
+function onEditTodo(todoId) {
+    const todo = ProjectManager.getTodo(activeProjectId, todoId);
+    UI.showTodoModal(todo);
+}
+
+function onDeleteTodo(todoId) {
+    ProjectManager.deleteTodo(activeProjectId, todoId);
+    renderAll();
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     // On page load, set the first project as active and render everything
     let initialProjects = ProjectManager.getProjects();
@@ -37,9 +66,18 @@ document.addEventListener('DOMContentLoaded', () => {
         // Create a default project if none exist
         ProjectManager.addProject('Default Project');
         initialProjects = ProjectManager.getProjects();
+    }
 
         activeProjectId = initialProjects[0].id;
         renderAll();
-    }
+
+        UI.setupEventListeners({
+            onAddProject,
+            onSelectProject,
+            onAddTodo,
+            onSaveTodo,
+            onEditTodo,
+            onDeleteTodo,
+        });
 });
 
