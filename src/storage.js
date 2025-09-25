5418428105fe78eb52f5b8062046d82346ce7658
Ford Berry <fordaberry@gmail.com>
2025-09-24 22:43:56 -0600

    refactor: Convert projects into project instances

--- src/storage.js
+++ src/storage.js
@@ -1,10 +1,12 @@
+import Project from './Project.js';
+
 const getStoredProjects = () => {
     const projectsJSON = localStorage.getItem('projects');
     if (projectsJSON) {
         try {
-            const parsedProjects = JSON.parse(projectsJSON);
-            if (Array.isArray(parsedProjects)) {
-                return parsedProjects;
+            const plainProjects = JSON.parse(projectsJSON);
+            if (Array.isArray(plainProjects)) {
+                return plainProjects.map(plainProject => Project.fromPlainObject(plainProject));
             }
         } catch (e) {
             console.error("Failed to parse projects from localStorage", e);
